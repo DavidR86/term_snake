814009b21d9a798bf23c3d9387cf33339f8e5d9b
DavidR86 <44814450+DavidR86@users.noreply.github.com>
2020-10-21 14:33:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ ls /dev/input/by-id/
 echo -e "\nPlease copy the name of your keyboard from those appearing in this list, exactly as it is written, and press enter. \n"
 read keyboard
 echo -e "Creating symbolic link to keyboard stream..."
-ln -s -f -v /dev/input/by-id/$keyboard ./keyboard
+ln -s -f -v /dev/input/by-id/$keyboard ./symlink/keyboard
 echo -e "Now the file permissions have to be set to read using chmod (sudo is required, you can the permissions back after done running the game)"
 echo -e "If you don't trust the script, you can run it with a text editor to see what it is doing."
 echo -e "Please enter your password. It will be invisible as you type it..."
-sudo chmod o+r ./keyboard
+sudo chmod o+r ./symlink/keyboard
 echo - "Done! Now you should be able to play the game. If you cannot move the character, there was likely an error with this installer."
